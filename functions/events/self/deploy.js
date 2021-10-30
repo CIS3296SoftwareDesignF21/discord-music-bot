27cb991e5b6ea1e420e9922d77425a30bda03e1a
Fay <73084642+avariantphi@users.noreply.github.com>
2021-10-30 12:26:51 -0400

    Update deploy.js
    added STDLIB_SECRET_TOKEN as a repository secret, change the config to point to it's new location.

--- functions/events/self/deploy.js
+++ functions/events/self/deploy.js
@@ -1,6 +1,6 @@
 // authenticates you with the API standard library
 // type `await lib.` to display API autocomplete
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+const lib = require('lib')({token: ${{secrets.STDLIB_SECRET_TOKEN}});
 
 await lib.discord.channels['@0.2.0'].messages.create({
   channel_name: `#general`,
