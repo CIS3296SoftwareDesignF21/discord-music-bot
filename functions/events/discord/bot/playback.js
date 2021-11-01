ecdc7ccd58003f97d89862d87228177d6b09f996
Fay <73084642+avariantphi@users.noreply.github.com>
2021-11-01 13:44:05 -0400

    update playback.js
    add in third party code building std lib

--- functions/events/discord/bot/playback.js
+++ functions/events/discord/bot/playback.js
@@ -1,3 +1,4 @@
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const helpers = require('../../../../helpers/shared.js');
 
 let nextTrack = await helpers.dequeueTrack(context.params.event);
