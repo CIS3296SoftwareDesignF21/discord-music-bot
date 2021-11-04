5b7722f387a04a38d0183b39eac4741bea31e1d2
Fay <73084642+avariantphi@users.noreply.github.com>
2021-11-04 10:10:30 -0400

    Update index.js
    simplify to hello-world output

--- index.js
+++ index.js
@@ -4,16 +4,9 @@ const { token } = require('./env.json');
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
-
+// Login to Discord with your client's token
 client.login(token);
