c72c53c6efbc09023ed3072229923f6fe8ff45d6
Fay Johnston <swewrozka@gmail.com>
2021-11-08 17:32:33 -0500

    hook up cmds->bot-server

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,58 @@
 const fs = require('fs');
-const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const { CommandOptionType, Command } = require('slash-create');
 require("dotenv").config();
 
-const commands = []
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commands = [
+	{
+		name: "play",
+		description: "Plays a song!",
+		options: [
+			{
+				name: "query",
+				type: CommandOptionType.STRING,
+				description: "The song you want to play",
+				required: true
+			}
+		]
+	},
+	{
+		name: "pause",
+		description: "Pause a song!",
+		options: [
+			{
+				name: "query",
+				type: CommandOptionType.STRING,
+				description: "The song you want to pause",
+				required: true
+			}
+		]
+	}
+];
+// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
+const clientId = process.env.clientId;
+const guildId = process.env.guildId;
+
+// for (const file of commandFiles) {
+// 	const command = require(`./commands/${file}`);
+// 	commands.push(command.data.toJSON());
+// }
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.clientId, process.env.guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
