f75617cda2621e6722b802e587b636f25f17fa2b
Fay Johnston <swewrozka@gmail.com>
2021-11-13 08:53:30 -0500

    fixes #23

--- commands/pause.js
+++ commands/pause.js
@@ -1,20 +1,12 @@
 const { QueryType } = require('discord-player');
-const { SlashCommand, CommandOptionType } = require('slash-create');
+const { SlashCommand} = require('slash-create');
 
 module.exports = class extends SlashCommand{
 	
 	constructor(creator) {
 		super(creator, {
 			name: 'pause',
-			aliases: ['s'],
 			description: 'Pause a song',
-			options: [ {
-				name: 'query',
-				type: CommandOptionType.STRING,
-				description: 'The song you want to pause',
-				required: true
-			} ],
-			
 			guildIDs: process.env.DISCORD_GUILD_ID ? [ process.env.DISCORD_GUILD_ID ] : undefined
 		});	
 	}
@@ -24,37 +16,10 @@ module.exports = class extends SlashCommand{
 		const { client } = require('..');
 		await ctx.defer();
 		
-		const guild = client.guilds.cache.get(ctx.guildID);
-		const channel = guild.channels.cache.get(ctx.channelID);
-		const query = ctx.options.query;
-		const searchResult = await client.player
-		
-		.search(query, {
-			requestedBy: ctx.user,
-			searchEngine: QueryType.AUTO
-			})
-			
-		.catch(() => {
-			console.log('misuta Robotto');
-		});
-		
-		if (!searchResult || !searchResult.tracks.length) return void ctx.sendFollowUp({ content: 'No results were found!' });
-		
-		const queue = await client.player.createQueue(guild, {
-			metadata: channel
-		});
-		
-		const member = guild.members.cache.get(ctx.user.id) ?? await guild.members.fetch(ctx.user.id);
-		try {
-			if (!queue.connection) await queue.connect(member.voice.channel);
-		} catch {
-			void client.player.getQueue(ctx.guildID);
-			return void ctx.sendFollowUp({ content: 'Could not join your voice channel!' });
-		}
-		
+		const queue = await client.player.getQueue(ctx.guildID);
+		if (!queue || !queue.playing) return void ctx.sendFollowUp({ content: '? | No music is being played!' });
 		const paused = queue.setPaused(true);
 		await ctx.sendFollowUp({ content: paused ? "⏸ | Paused!" : "❌ | Something went wrong!" });
-		if (!queue || !queue.playing) await queue.stop();
 		
 	}
 	
